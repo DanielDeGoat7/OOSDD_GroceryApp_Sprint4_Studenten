c80f7349201db5ed699e167d3eb6613a593d57df
Daniel <s1213686@student.windesheim.nl>
2025-10-02 18:25:06 +0200

    feat: Client class aangepast met Role

--- Grocery.Core/Models/Client.cs
+++ Grocery.Core/Models/Client.cs
@@ -6,10 +6,11 @@ namespace Grocery.Core.Models
         public string EmailAddress { get; set; }
         public string Password { get; set; }
         public Role Role { get; set; } = Role.None;
-        public Client(int id, string name, string emailAddress, string password) : base(id, name)
+        public Client(int id, string name, string emailAddress, string password, Role role = Role.None) : base(id, name)
         {
             EmailAddress=emailAddress;
             Password=password;
+            Role = role;
         }
     }
 }
