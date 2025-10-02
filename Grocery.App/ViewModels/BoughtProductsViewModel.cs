46d633d4e86c255ba048a96cde0f214a7559a1c0
Daniel <s1213686@student.windesheim.nl>
2025-10-02 15:56:44 +0200

    feat: OnSelectedProductChanged uitgewerkt

--- Grocery.App/ViewModels/BoughtProductsViewModel.cs
+++ Grocery.App/ViewModels/BoughtProductsViewModel.cs
@@ -25,6 +25,16 @@ namespace Grocery.App.ViewModels
         partial void OnSelectedProductChanged(Product? oldValue, Product newValue)
         {
             //Zorg dat de lijst BoughtProductsList met de gegevens die passen bij het geselecteerde product. 
+            BoughtProductsList.Clear();
+
+            if (newValue != null)
+            {
+                var boughtProducts = _boughtProductsService.Get(newValue.Id);
+                foreach (var bp in boughtProducts)
+                {
+                    BoughtProductsList.Add(bp);
+                }
+            }
         }
 
         [RelayCommand]
